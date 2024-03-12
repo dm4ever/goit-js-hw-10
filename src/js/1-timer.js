2c3c4fc4c681505a8b649696664533e4ff79c301
dmfanua <h.mudrytskyi@gmail.com>
2024-03-12 17:12:50 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/1-timer.html 	modified:   src/2-snackbar.html 	modified:   src/js/1-timer.js

---
+++ src/js/1-timer.js
@@ -0,0 +1,6 @@
+// const flatpickr = require('flatpickr');
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const startButton = document.querySelector((type = 'button'));
+const form = document.querySelector('#datetime-picker');

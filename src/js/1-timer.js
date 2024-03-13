3f876444659531142b823d442c172d358134f1f4
dmfanua <h.mudrytskyi@gmail.com>
2024-03-13 16:35:47 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/js/1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,20 @@
 // const flatpickr = require('flatpickr');
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const startButton = document.querySelector((type = 'button'));
-const form = document.querySelector('#datetime-picker');
+const form = document.querySelector(input[(type = 'text')]);
+
+flatpickr(selector, options) {
+    const options = {
+        enableTime: true,
+        time_24hr: true,
+        defaultDate: new Date(),
+        minuteIncrement: 1,
+        onClose(selectedDates) {
+            console.log(selectedDates[0]);
+        },
+    }
+};
